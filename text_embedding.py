8a2b32d15dacc4be2758e655a799934593784fae
Bob Poekert <bob@poekert.com>
2019-03-09 07:06:45 -0800

    weight svd dims higher than bow dims

--- text_embedding.py
+++ text_embedding.py
@@ -1,6 +1,7 @@
 import numpy as np
 import mxnet as mx
 from mxnet import nd, autograd, gluon, gpu
+from mxnet.gluon.loss import _reshape_like, _apply_weighting
 import struct
 import gzip
 from sklearn.model_selection import train_test_split
@@ -74,6 +75,57 @@ class BOWReader(object):
         while 1:
             yield self.outp_queue.get()
 
+class WeightedHuberLoss(gluon.loss.Loss):
+    r"""Calculates smoothed L1 loss that is equal to L1 loss if absolute error
+    exceeds rho but is equal to L2 loss otherwise. Also called SmoothedL1 loss.
+
+    .. math::
+        L = \sum_i \begin{cases} \frac{1}{2 {rho}} ({label}_i - {pred}_i)^2 &
+                           \text{ if } |{label}_i - {pred}_i| < {rho} \\
+                           |{label}_i - {pred}_i| - \frac{{rho}}{2} &
+                           \text{ otherwise }
+            \end{cases}
+
+    `label` and `pred` can have arbitrary shape as long as they have the same
+    number of elements.
+
+    Parameters
+    ----------
+    rho : float, default 1
+        Threshold for trimmed mean estimator.
+    weight : float or None
+        Global scalar weight for loss.
+    batch_axis : int, default 0
+        The axis that represents mini-batch.
+
+
+    Inputs:
+        - **pred**: prediction tensor with arbitrary shape
+        - **label**: target tensor with the same size as pred.
+        - **sample_weight**: element-wise weighting tensor. Must be broadcastable
+          to the same shape as pred. For example, if pred has shape (64, 10)
+          and you want to weigh each sample in the batch separately,
+          sample_weight should have shape (64, 1).
+        - **dim_weights**: vector that abs(a - b) is multiplied by before we take its norm.
+          For when loss in some dimensions is more imporatnt than others.
+
+    Outputs:
+        - **loss**: loss tensor with shape (batch_size,). Dimenions other than
+          batch_axis are averaged out.
+    """
+    def __init__(self, rho=1, weight=None, batch_axis=0, dim_weights=None, **kwargs):
+        super(WeightedHuberLoss, self).__init__(weight, batch_axis, **kwargs)
+        self.dim_weights = dim_weights
+        self._rho = rho
+
+    def hybrid_forward(self, F, pred, label, sample_weight=None):
+        label = _reshape_like(F, label, pred)
+        loss = F.abs(label - pred)
+        loss = loss * self.dim_weights
+        loss = F.where(loss > self._rho, loss - 0.5 * self._rho,
+                       (0.5/self._rho) * F.square(loss))
+        loss = _apply_weighting(F, loss, self._weight, sample_weight)
+        return F.mean(loss, axis=self._batch_axis, exclude=True)
 
 class TextEmbedding(gluon.Block):
 
@@ -81,15 +133,11 @@ class TextEmbedding(gluon.Block):
         super(TextEmbedding, self).__init__(**kwargs)
         with self.name_scope():
             self.input = gluon.nn.Dense(n_words, activation='relu')
-            self.input_squish_1 = gluon.nn.Dense(int(n_words / 2), activation='relu')
-            self.input_squish_2 = gluon.nn.Dense(int(n_words / 4), activation='relu')
             self.input_squish_3 = gluon.nn.Dense(512, activation='relu')
             self.outp = gluon.nn.Dense(vec_dims + n_words)
 
     def forward(self, x):
         x = self.input(x)
-        x = self.input_squish_1(x)
-        x = self.input_squish_2(x)
         x = self.input_squish_3(x)
         x = self.outp(x)
         return x
@@ -110,7 +158,10 @@ def fit_text_embedding(data_fname, params_fname, ctx, epochs):
 
     reader = BOWReader(data_fname, batch_size=1000)
 
-    l1_loss = gluon.loss.HuberLoss()
+    loss_weights = np.ones((vec_dims + n_words),)
+    loss_weights[n_words:] = 20
+    loss_weights = nd.array(loss_weights).as_in_context(ctx)
+    l1_loss = WeightedHuberLoss(dim_weights=loss_weights)
 
     holdout_size = 100
 
@@ -151,6 +202,9 @@ def fit_text_embedding(data_fname, params_fname, ctx, epochs):
             epoch += 1
             if epoch > epochs:
                 break
+
+            if epoch % 10 == 0:
+                net.save_parameters(params_fname)
 
     finally:
         net.save_parameters(params_fname)
