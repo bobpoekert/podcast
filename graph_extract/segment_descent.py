aa1aa1413e23b11811ddc1fc09d9b403dfe71865
bob <bob@poekert.com>
2019-07-10 09:15:20 -0700

    update segment_descent

--- graph_extract/segment_descent.py
+++ graph_extract/segment_descent.py
@@ -1,20 +1,24 @@
 from segment import make_clusters
 import subprocess as sp
-import sys
+import sys, os
 import numpy as np
 from threading import Thread, Semaphore 
 from multiprocessing import cpu_count
 from functools import partial
 
-pool_sem = Semaphore(value=cpu_count())
+pool_sem = Semaphore(value=cpu_count()*1.5)
 
 def run(job):
     def _run():
-        with pool_sem:
+        try:
             job()
+        finally:
+            pool_sem.release()
+    pool_sem.acquire()
     thread = Thread(target=_run)
     thread.daemon = True 
     thread.start()
+    return thread
 
 dists_fname = sys.argv[1]
 fname_2d = sys.argv[2]
@@ -23,11 +27,11 @@ mat_2d = np.fromfile(fname_2d, dtype=np.float32).reshape((-1, 2))
 n_topics = mat_2d.shape[0]
 topics_bbox = (
     np.min(mat_2d[:, 0]), np.min(mat_2d[:, 1]),
-    np.max(mat_2d(:, 0]), np.max(mat_2d[:, 1])
+    np.max(mat_2d[:, 0]), np.max(mat_2d[:, 1])
 )
 
 n_levels = 5
-image_size_px = (1280, 1280)
+image_size_px = (800, 800)
 tile_size_px = (300, 300)
 tiles_per_screen = (
     image_size_px[0] / tile_size_px[0],
@@ -39,32 +43,53 @@ def slice_bboxes(bbox):
     bbox_height = bbox[3] - bbox[1]
     step_x = bbox_width / tiles_per_screen[0]
     step_y = bbox_height / tiles_per_screen[1]
-    for x_off in range(bbox[0], bbox[2], bbox_step_x):
-        for y_off in range(bbox[1], bbox[3], bbox_step_y):
+    for x_off in np.linspace(bbox[0], bbox[2]):
+        for y_off in np.linspace(bbox[1], bbox[3]):
             yield (x_off, y_off, x_off + step_x, y_off + step_y)
 
 def make_prefix(bbox):
-    return "%f_%f_%f_%f" % bbox
+    b = "%f_%f_%f_%f" % bbox
+    return "%s_%d_%d" % (b, image_size_px[0], image_size_px[1])
+
+def call(cmd):
+    print(' '.join(cmd))
+    return sp.check_call(cmd)
+
+def partition(things, partition_size):
+    return [things[start:(start + partition_size)] for start in range(0, len(things), partition_size)]
 
 def make_raster(bbox, is_parallel=False):
     min_x, min_y, max_x, max_y = bbox
     prefix = make_prefix(bbox)
-    sp.check_call(['./word_map.exe', dists_fname, fname_2d,
-        '%s_dists.bin' % prefix,
-        tile_size_px[0], tile_size_px[1], min_x, min_y, max_x, max_y,
-        'true' if is_parallel else 'false'])
+    fname = '/mnt/lappy2/tiles/%s_dists.bin' % prefix
+    if not os.path.exists(fname):
+        call(['./word_map.exe', dists_fname, fname_2d,
+            str(image_size_px[0]), str(image_size_px[1]),
+            fname,
+            str(min_x), str(min_y), str(max_x),str(max_y),
+            'true' if is_parallel else 'false'])
 
 def quickshift(bbox):
     prefix = make_prefix(bbox)
-    labels = make_clusters('%s_dists.bin' % prefix)
-    labels.tofile('%s_labels.bin' % prefix)
+    fname = '/mnt/lappy2/tiles/%s_labels.bin' % prefix
+    if not os.path.exists(fname):
+        labels = make_clusters('/mnt/lappy2/tiles/%s_dists.bin' % prefix, image_size_px[0], image_size_px[1])
+        labels.tofile(fname)
+
+def make_tile_tree_from_slice(part, level):
+    for bbox in part:
+        make_tile_tree(bbox, level=level)
 
 def make_tile_tree(bbox, level=1):
     make_raster(bbox, is_parallel=(level == 1))
     quickshift(bbox)
     if level < n_levels:
-        for slice_bbox in slice_bboxes(bbox):
-            run(partial(make_tile_tree, slice_bbox, level=level+1))
+        threads = []
+        for part in slice_bboxes(bbox):
+            threads.append(run(partial(make_tile_tree, part, level=level+1)))
+        print('threads', len(threads))
+        for thread in threads:
+            thread.join()
 
 if __name__ == '__main__':
     make_tile_tree(topics_bbox)
